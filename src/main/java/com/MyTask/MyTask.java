7974d95ab3281faaa1cd22051a461398d6dcd28f
panghaichen <panghaichen@jd.com>
2018-08-02 11:11:05 +0800

    定时调度：使用Spring Task的一个小Demo

---
+++ src/main/java/com/MyTask/MyTask.java
@@ -0,0 +1,15 @@
+package com.MyTask;
+
+/**
+ * Created by panghaichen on 2018-08-02 11:00
+ **/
+public class MyTask {
+    public void show() {
+        System.out.println(System.currentTimeMillis()+":show method 1");
+    }
+
+    public void print() {
+        System.out.println(System.currentTimeMillis()+":print method 1");
+    }
+
+}
