c91f091576ceeeb0a3e0f0e5f575b66747f066c7
NullTommy <1271033085@qq.com>
2018-12-19 15:18:53 +0800

    log4j配置和打印优化

--- src/main/java/com/TestMain/TestLog.java
+++ src/main/java/com/TestMain/TestLog.java
@@ -14,16 +14,17 @@ public class TestLog {
 class Test {
     //注意这两个方法的不同。里面的参数："Test"和Test.class实际对应的是log4j.xml的logger的name属性（String类型）。
     final Logger log = Logger.getLogger(Test.class);
+    //final Logger log = Logger.getLogger("Test");
     public void test() {
         log.debug("hello this is log4j debug log");
-        log.info("hello this is log4j info log");
-        log.warn("hello this is log4j warn log");
+        log.info( "hello this is log4j info log");
+        log.warn( "hello this is log4j warn log");
         log.error("hello this is log4j error log");
         log.fatal("hello this is log4j fatal log");
 
         log.log(DEBUG,"hello this is log4j DEBUG log");
-        log.log(INFO,"hello this is log4j INFO log");
-        log.log(WARN,"hello this is log4j WARN log");
+        log.log(INFO, "hello this is log4j INFO log");
+        log.log(WARN, "hello this is log4j WARN log");
         log.log(ERROR,"hello this is log4j ERROR log");
         log.log(FATAL,"hello this is log4j FATAL log");
     }
