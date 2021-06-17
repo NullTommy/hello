db7485d51d7d9b84b93d3fd25922b9bc23f23a31
NullTommy <NullTommy@163.com>
2021-06-17 13:43:02 +0800

    RE-000:1、简单的HTTP请求使用；

---
+++ src/main/java/com/TestArch/TestHttp.java
@@ -0,0 +1,47 @@
+package com.TestArch;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * 功能描述: <br>
+ * 〈〉
+ *  [参考: Java工具类--通过HttpClient发送http请求_程高伟-CSDN博客_httpclient发送请求 https://blog.csdn.net/frankcheng5143/article/details/50070591 ]
+ * @return:
+ * @since: 1.0.0
+ * @Date: 2021/6/17 0017 13:40
+ */
+public class TestHttp {
+    public static void main(String args[]) throws IOException {
+
+        //1.获得一个httpclient对象
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        //2.生成一个get请求
+        HttpGet httpget = new HttpGet("http://www.baidu.com/");
+        //3.执行get请求并返回结果
+        CloseableHttpResponse response = httpclient.execute(httpget);
+
+        try {
+            //4.处理结果
+            String result = null;
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity,"UTF-8");
+            System.out.println("" + result);
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
